d09b8fb315163ae6e5bb7075e98cc01a351e7476
Devesh Sahu <greenwoods33@gmail.com>
2016-05-01 19:52:17 -0500

    Everything fixed but server response

--- Cam2/app/src/main/java/sahu/devesh/cam2/MainActivity.java
+++ Cam2/app/src/main/java/sahu/devesh/cam2/MainActivity.java
@@ -5,12 +5,18 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
+import android.renderscript.ScriptGroup;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -23,12 +29,31 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.internal.http.multipart.MultipartEntity;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.InputStreamEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.ByteArrayBuffer;
+
+import java.io.BufferedInputStream;
+import java.io.DataOutput;
 import java.io.File;
 import java.io.FileDescriptor;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
+import java.net.URLConnection;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -38,14 +63,26 @@ public class MainActivity extends AppCompatActivity {
     private ImageView preview;
     private Uri fileUri = null;
 
+    private Bitmap bitmap = null;
+    private Bitmap outputBitmap = null;
+
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
 
     public static final int MEDIA_TYPE_IMAGE = 1;
     public static final int PICK_IMAGE = 2;
     public static final int UPLOAD_IMAGE = 3;
 
-    public static String server = "http://104.236.112.86/";
+    // Tracks user action (Capture or Pick)
+    public static int choice = 0;
 
+    // Server Location
+    public String server = "http://104.236.112.86/";
+
+    // Absolute path of the source Image
+    public String sourceImagePath;
+    public String destinationImagePath;
+
+    // Create a file to store Captured Image
     private static Uri getOutputMediaFileUri (int type){
         return Uri.fromFile(getOutputMediaFile(type));
     }
@@ -76,15 +113,67 @@ public class MainActivity extends AppCompatActivity {
         return mediaFile;
     }
 
-    private void getBitmapFromUri(Uri uri) {
+    // Convert the file into Grayscale image and save it again
+    private void convertToGrayScale(String path){
+        File image = new File(path);
+        Bitmap original = BitmapFactory.decodeFile(image.getAbsolutePath());
+        int width, height;
+        height = original.getHeight();
+        width = original.getWidth();
+
+        Bitmap grayScale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(grayScale);
+        Paint paint = new Paint();
+        ColorMatrix cm = new ColorMatrix();
+        cm.setSaturation(0);
+        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+        paint.setColorFilter(f);
+        c.drawBitmap(original, 0, 0, paint);
+
+        try {
+            OutputStream fOut = new FileOutputStream(image);
+
+            grayScale.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
+            fOut.flush();
+            fOut.close();
+
+            MediaStore.Images.Media.insertImage(getContentResolver(), image.getAbsolutePath(), image.getName(), image.getName());
+
+        }catch (Exception e) {
+        }
+    }
+
+    // Resample to fit the image view
+    private void resetImageforView(String path){
+
+        File image = new File(path);
+
+        Bitmap temp = BitmapFactory.decodeFile(image.getAbsolutePath());
+        temp = Bitmap.createScaledBitmap(temp,1024, 768,true);
+        preview.setImageBitmap(temp);
+    }
+
+   // Copy the image from Uri to a new location
+   // Save the absolute path of this file in sourceImagePath
+   private void saveUriToFile(Uri uri) {
         ParcelFileDescriptor parcelFileDescriptor = null;
         try {
             parcelFileDescriptor = getContentResolver().openFileDescriptor(uri, "r");
             FileDescriptor imageSource = parcelFileDescriptor.getFileDescriptor();
 
-            Bitmap image = BitmapFactory.decodeFileDescriptor(imageSource);
+            bitmap = BitmapFactory.decodeFileDescriptor(imageSource);
 
-            preview.setImageBitmap(image);
+            String path = Environment.getExternalStorageDirectory().toString();
+            OutputStream fOut = null;
+            File file = new File (path, "imageUploaded" + ".jpg");
+            sourceImagePath = file.getAbsolutePath();
+            fOut = new FileOutputStream(file);
+
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
+            fOut.flush();
+            fOut.close();
+
+            MediaStore.Images.Media.insertImage(getContentResolver(), file.getAbsolutePath(),file.getName(),file.getName());
 
         }catch (FileNotFoundException e) {
         }catch (IOException e){
@@ -97,6 +186,128 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // Upload raw image and download/save colorized image in a separate thread
+    private class talkToServer extends AsyncTask<ImageView, Void, String>{
+
+        ImageView imageView;
+
+        protected String doInBackground(ImageView... params){
+
+            this.imageView = params[0];
+
+            File file = new File(sourceImagePath);
+
+            try {
+                HttpClient httpClient = new DefaultHttpClient();
+
+                HttpPost httpPost = new HttpPost(server);
+
+                InputStreamEntity reqEntity = new InputStreamEntity(
+                        new FileInputStream(file), -1);
+                reqEntity.setContentType("binary/octet-stream");
+                reqEntity.setChunked(true);
+                httpPost.setEntity(reqEntity);
+                HttpResponse response = httpClient.execute(httpPost);
+
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                        + "/CameraApp/" + "Col_IMG_" + timeStamp + ".jpg" ;
+
+                try {
+                    HttpEntity entity = response.getEntity();
+                    if (entity != null){
+                        InputStream inputStream = entity.getContent();
+                        FileOutputStream outputStream = new FileOutputStream(path);
+                        int bufferSize = 1024;
+                        byte[] buffer = new byte[bufferSize];
+                        int len = 0;
+                        while ((len = inputStream.read(buffer)) != -1){
+                            outputStream.write(buffer, 0, len);
+                        }
+                        outputStream.close();
+                    }
+                    destinationImagePath = path;
+
+                } catch (Exception e){
+                    e.printStackTrace();
+                    Log.d("response ", "Problems with recieved response");
+                }
+
+               // outputBitmap = BitmapFactory.decodeStream(path);
+
+
+               /* try {
+                    OutputStream fOut = null;
+                    File outFile = new File (path);
+                    fOut = new FileOutputStream(outFile);
+
+                    outputBitmap.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
+                    fOut.flush();
+                    fOut.close();
+
+                    MediaStore.Images.Media.insertImage(getContentResolver(), outFile.getAbsolutePath(),outFile.getName(),outFile.getName());
+
+                    destinationImagePath = path;
+
+                }catch (Exception e) {
+                    e.printStackTrace();
+                    Log.d("outstream ", "Could not save output file");
+                }*/
+
+            } catch (Exception e){
+
+                e.printStackTrace();
+
+                Log.d("response","connectivity failed");
+            }
+
+            //resetImageforView(destinationImagePath);
+            return "True";
+        }
+
+        protected void postExecute(){
+            imageView.setImageBitmap(outputBitmap);
+        }
+    }
+
+    /* Put your comment here
+
+    private void downloadImage() throws IOException{
+        InputStream in = null;
+        int response = -1;
+
+        URL url = new URL(server);
+        URLConnection conn = url.openConnection();
+
+        if (! (conn instanceof HttpURLConnection)) {
+            throw new IOException("Not an HTTP connection");
+        }
+
+        try{
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+            response = httpConn.getResponseCode();
+            if (response == HttpURLConnection.HTTP_OK) {
+                in = httpConn.getInputStream();
+
+                BufferedInputStream bis = new BufferedInputStream(in, 3*2048);
+
+                ByteArrayBuffer baf = new ByteArrayBuffer(50);
+                int current = 0;
+                while ((current = bis.read()) != -1){
+                    baf.append((byte)current);
+                }
+
+                byte[] imageData = baf.toByteArray();
+                outputBitmap = BitmapFactory.decodeByteArray(imageData, 0, imageData.length);
+            }
+
+        } catch (Exception ex){
+            throw new IOException("Error Connecting");
+        }
+    }*/
 
 
     @Override
@@ -109,18 +320,22 @@ public class MainActivity extends AppCompatActivity {
         browse = (Button) findViewById(R.id.browse);
         upload = (Button) findViewById(R.id.upload);
 
+        // Open Camera Intent to take picture
         cam.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
                 fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+                sourceImagePath = fileUri.getPath();
+
                 intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
 
                 startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
             }
         });
 
+        // Open Gallary to choose picture
         browse.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -131,10 +346,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Upload picture to the server and download colorized one
         upload.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
 
+                Log.d("upload", sourceImagePath);
+
+                new talkToServer().execute(preview);
 
             }
         });
@@ -143,29 +362,39 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        // Call back for captured image
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
-                Toast.makeText(this, "Image saved to: " + data.getData(), Toast.LENGTH_LONG).show();
-                getBitmapFromUri(fileUri);
+
+                convertToGrayScale(sourceImagePath);
+
+                resetImageforView(sourceImagePath);
+
             }
 
             else if (resultCode == RESULT_CANCELED) {
+                // Print cancelled
             }
 
             else {
+                // Do nothing
             }
         }
 
+        // Call back for picked image
         else if (requestCode == PICK_IMAGE){
             if (data != null) {
                 fileUri = data.getData();
-                Log.i("CamApp", "Uri: " + fileUri.toString());
-                getBitmapFromUri(fileUri);
+
+                Log.i("Browse ", "Uri " + fileUri.toString());
+
+                saveUriToFile(fileUri);
+
+                convertToGrayScale(sourceImagePath);
+
+                resetImageforView(sourceImagePath);
+
             }
-        }
-
-        else if (requestCode == UPLOAD_IMAGE){
-
         }
     }
 
