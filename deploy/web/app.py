76171332e9ca47d37e6895f30cc9c635579c89d2
Tyler Folkman <tylerfolkman@gmail.com>
2016-05-02 10:15:28 -0500

    have working with cleaner interface

--- deploy/web/app.py
+++ deploy/web/app.py
@@ -1,47 +1,37 @@
 import os
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, render_template
 from werkzeug import secure_filename
 from utils import colorize
 from flask import send_file
 import scipy.misc
+import json
 
-UPLOAD_FOLDER = './tmp/'
-ALLOWED_EXTENSIONS = set(['jpg'])
+UPLOAD_FOLDER = './static/images'
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+           filename.lower().rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route("/", methods=['GET', 'POST'])
+
+@app.route("/process_photo/", methods=['POST'])
+def process_photo():
+    file = request.files['bwPhoto']
+    if file and allowed_file(file.filename):
+        file.save(UPLOAD_FOLDER + "/bw.jpg")
+        color_photo = colorize.run_color(UPLOAD_FOLDER + "/bw.jpg")
+        scipy.misc.imsave(UPLOAD_FOLDER + "/color.jpg", color_photo)
+        #return json.dumps("okay")
+        return send_file(UPLOAD_FOLDER + "/color.jpg", mimetype='image/jpg')
+
+
+@app.route("/", methods=['GET'])
 def index():
-    if request.method == 'POST':
-	f = open(UPLOAD_FOLDER + "logs.txt", "w")
-	f.write("rcvd file\n")
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            file.save(UPLOAD_FOLDER + "bw.jpg")
-            f.write("saved file\n")
-	    f.close()
-            color_photo = colorize.run_color(UPLOAD_FOLDER + "bw.jpg")
-	    f = open(UPLOAD_FOLDER + "logs.txt", "a")
-            f.write("color file\n")
-            scipy.misc.imsave(UPLOAD_FOLDER + "color.jpg", color_photo)
-            f.write("saved color file\n")
-	    f.close()
-	    return send_file(UPLOAD_FOLDER + "color.jpg", mimetype='image/jpg')
-    return """
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    <p>%s</p>
-    """ % "<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],))
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
